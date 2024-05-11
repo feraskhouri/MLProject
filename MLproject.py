aa23927bf8db07bb45d675eace7d41feac2e3f28
feraskhouri <90791106+feraskhouri@users.noreply.github.com>
2024-05-11 17:12:11 +0300

    hot fix fot he gui

--- MLproject.py
+++ MLproject.py
@@ -3,16 +3,31 @@ import numpy as np
 import pandas as pd
 import tkinter as tk
 import matplotlib.pyplot as plt
-from tkinter import messagebox, ttk
+from tkinter import messagebox,ttk
 from tkinter import filedialog
 from collections import Counter
-from scipy.stats import chi2_contingency 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from tkinter import *
+from tkinter import font
+from PIL import ImageTk,Image
+
+
 
 filename = ""
 df = pd.DataFrame()  
 
+def KNN(x,y,k,ss):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+    if ss:
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform(X_test)
+    classifier = KNeighborsClassifier(n_neighbors = k, metric = 'minkowski', p = 2)
+    classifier.fit(X_train, y_train)
 def upload_file():
-    global filename, df
+    global filename,df
     filename = filedialog.askopenfilename(filetypes=[("Excel files", "*.csv")])
     messagebox.showinfo("File Uploaded", f"File {filename} uploaded successfully.")
     df = pd.read_csv(filename)
@@ -21,24 +36,13 @@ def upload_file():
     column_menu['values'] = column_names
     x_var_menu['values'] = column_names
     y_var_menu['values'] = column_names
-
-
-
+    page2.tkraise()
 def box_plot():
-    A = column_select.get()  # Changed from entry.get()
-    df[A].fillna(0, inplace=True)
+    A = column_select.get()  # Changed from entry.get()    df[A].fillna(0, inplace=True)
     numbers = df[A].to_list()
     fig, ax = plt.subplots()
     ax.boxplot(numbers)
-    ax.set_title('Box Plot of Numbers')
-    plt.show()
-
-    A = column_select.get()  # Changed from entry.get()
-    df[A].fillna(0, inplace=True)
-    numbers = df[A].to_list()
-    fig, ax = plt.subplots()
-    ax.boxplot(numbers)
-    ax.set_title('Box Plot of Numbers')
+    ax.set_title(f'Box Plot of {A}')
     plt.show()
 def median_smoothing(data, num_bins):
     bin_size = len(data) // num_bins
@@ -93,8 +97,11 @@ def z_score_normalization(data):
     return normalized_zScoure
 def normalization_by_decimal_scaling(data):
     maximum = max(data)
-    magnitude = len(str(int(maximum))) - 1
-    pow_ = 10 ** magnitude
+    minimum = min(data)
+    magnitude1 = len(str(int(maximum))) - 1
+    magnitude2 = len(str(int(minimum))) - 1
+    j=max(magnitude1,magnitude2)
+    pow_ = 10 ** j
     normalized_decScal = [x / pow_ for x in data]
     return normalized_decScal
 def calculate():
@@ -222,92 +229,119 @@ def perform_linear_regression():
     plt.ylabel(y_var)
     plt.show()
 
+#-----------------------root-------------------#
+root = Tk()
+root.iconbitmap("D:\MLProject\DML.jpg")
+root.title("DML")
+root.geometry('700x600')
+root.resizable(False,False)
 
-root = tk.Tk()
-root.title("Data Preprocessing")
+page1=Frame(root)
+page2=Frame(root)
+page3=Frame(root)
+page1.grid(row=0,column=0,sticky="nsew")
+page2.grid(row=0,column=0,sticky="nsew")
+page3.grid(row=0,column=0,sticky="nsew")
 
-notebook = ttk.Notebook(root)
+sty1=font.Font(size=18,slant="italic",underline=1)
+sty2=font.Font(size=12)
+page1.tkraise()
+#----------------------------page1-------------------------------#
+logopath = Image.open("D:\MLProject\DML.jpg")
+resize_dml = logopath.resize((400, 150))
+dml_logo = ImageTk.PhotoImage(resize_dml)
+dml_label = Label(page1, image=dml_logo)
+dml_label.grid(row=0, column=0, padx=150, pady=45)
 
-# Create frames for each tab
-tab1 = ttk.Frame(notebook)
-tab2 = ttk.Frame(notebook)
+labelintro = tk.Label(page1, text="Introduction",font=sty1)
+labelintro.grid(row=1, column=0, padx=10, pady=0)
 
-# Add the frames as tabs to the notebook
-notebook.add(tab1, text='Tab 1')
-notebook.add(tab2, text='Tab 2')
-button1 = tk.Button(tab1, text="Upload Excel File", command=upload_file)
-button1.pack(anchor='n')
-# Place the notebook on the window
-notebook.pack(expand=True, fill='both')
+labeltext = tk.Label(page1, text="First of all, we thank Dr. Mohamed Almsedin\n for putting his trust in us and helping us write this program\n, which we hope you will like and appreciate.\n We trust that it will assist you in performing various functions.\n In short, this program encompasses pre-processing \napplications in data mining and several machine learning algorithms.\n Therefore, we have named it DML, which stands for Data Mining - Machine Learning.",font=sty2)
+labeltext.grid(row=2, column=0, padx=10, pady=15)
 
-button_show_rows = tk.Button(tab1, text="Show data set", command=show_data)
+button_up = tk.Button(page1, text="Upload CSV file",font=sty2, command=upload_file)
+button_up.grid(row=3, column=0, padx=0, pady=30)
+
+button1t2 = tk.Button(page1, text="->", command=lambda: page2.tkraise())
+button1t2.grid(row=5, column=0, padx=0, pady=15)
+
+#----------------------------page2-----------------------------#
+
+button_show_rows = tk.Button(page2, text="Show data set", command=show_data)
 button_show_rows.pack(anchor='n')
 
-button_show_statistics = tk.Button(tab1, text="Show Statistics", command=show_statistics)
+button_show_statistics = tk.Button(page2, text="Show Statistics", command=show_statistics)
 button_show_statistics.pack()
+
+label = tk.Label(page2, text="Select the column:")
+label.pack(anchor='w')
+
 column_select = tk.StringVar()
-column_menu = ttk.Combobox(tab1, textvariable=column_select)
+column_menu = ttk.Combobox(page2, textvariable=column_select)
 column_menu.pack(anchor='w')
 
-
-labelsm = tk.Label(tab1, text="----Smoothing----")
-
+labelsm = tk.Label(page2, text="----Smoothing----")
 labelsm.pack(anchor='w')
 
 var5 = tk.IntVar()
-chk5 = tk.Checkbutton(tab1, text='Smoothing by mean', variable=var5)
+chk5 = tk.Checkbutton(page2, text='Smoothing by mean', variable=var5)
 chk5.pack(anchor='w')
 
 var6 = tk.IntVar()
-chk6 = tk.Checkbutton(tab1, text='Smoothing by median', variable=var6)
+chk6 = tk.Checkbutton(page2, text='Smoothing by median', variable=var6)
 chk6.pack(anchor='w')
 
 var7 = tk.IntVar()
-chk7 = tk.Checkbutton(tab1, text='Smoothing by bounders', variable=var7)
+chk7 = tk.Checkbutton(page2, text='Smoothing by bounders', variable=var7)
 chk7.pack(anchor='w')
 
-label_bin_size = tk.Label(tab1, text="Enter bin size for smoothing:")
+label_bin_size = tk.Label(page2, text="Enter bin size for smoothing:")
 label_bin_size.pack(anchor='w')
 
-entry_bin_size = tk.Entry(tab1)
+entry_bin_size = tk.Entry(page2)
 entry_bin_size.pack(anchor='w')
 
-labelno = tk.Label(tab1, text="----Normalization----")
+labelno = tk.Label(page2, text="----Normalization----")
 labelno.pack(anchor='w')
 
 var9 = tk.IntVar()
-chk9 = tk.Checkbutton(tab1, text='min max normalization', variable=var9)
+chk9 = tk.Checkbutton(page2, text='min max normalization', variable=var9)
 chk9.pack(anchor='w')
 
 var10 = tk.IntVar()
-chk10 = tk.Checkbutton(tab1, text='z score normalization', variable=var10)
+chk10 = tk.Checkbutton(page2, text='z score normalization', variable=var10)
 chk10.pack(anchor='w')
 
 var11 = tk.IntVar()
-chk11 = tk.Checkbutton(tab1, text='normalization by decimal scaling', variable=var11)
+chk11 = tk.Checkbutton(page2, text='normalization by decimal scaling', variable=var11)
 chk11.pack(anchor='w')
 
-buttonbox = tk.Button(tab1, text="Box Plot", command=box_plot)
+buttonbox = tk.Button(page2, text="Box Plot", command=box_plot)
 buttonbox.pack(anchor='w')
 
-button = tk.Button(tab1, text="Calculate", command=calculate)
+button = tk.Button(page2, text="Calculate", command=calculate)
 button.pack(anchor='w')
 
+button2t1 = tk.Button(page2, text="<-", command=lambda: page1.tkraise())
+button2t1.pack(anchor='n')
 
+button2t3 = tk.Button(page2, text="->", command=lambda: page3.tkraise())
+button2t3.pack(anchor='n')
 
-label = tk.Label(tab1, text="Enter name of column:")
-label.pack(anchor='w')  
-
+#-------------------------page3-----------------------#
 
 x_var_select = tk.StringVar()
 y_var_select = tk.StringVar()
-x_var_menu = ttk.Combobox(tab2, textvariable=x_var_select)
+x_var_menu = ttk.Combobox(page3, textvariable=x_var_select)
 x_var_menu.pack(anchor='w')
 
-y_var_menu = ttk.Combobox(tab2, textvariable=y_var_select)
+y_var_menu = ttk.Combobox(page3, textvariable=y_var_select)
 y_var_menu.pack(anchor='w')
-linear_regression_button = tk.Button(tab2, text="Perform Linear Regression", command=perform_linear_regression)
+linear_regression_button = tk.Button(page3, text="Perform Linear Regression", command=perform_linear_regression)
 linear_regression_button.pack(anchor='w')
 
+button3t2 = tk.Button(page3, text="<-", command=lambda: page2.tkraise())
+button3t2.pack(anchor='n')
 
-root.mainloop()   
+
+root.mainloop()
